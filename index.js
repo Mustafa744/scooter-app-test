62fff40aa683548dd1d16ab14c400591fbaa0980
mustafaashraf744 <mustafaashraf744@gmail.com>
2022-08-09 14:18:26 +0300

    my first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const { Server } = require("ws");
+
+const PORT = process.env.PORT || 3000;
+
+const server = express().listen(PORT, () =>
+  console.log(`Listening on ${PORT}`)
+);
+
+const wss = new Server({ server });
+
+wss.on("connection", (ws) => {
+  console.log("Client connected");
+  ws.on("close", () => console.log("Client disconnected"));
+});
